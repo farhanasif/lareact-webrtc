8520a6d03919cdb2e2f88da5bd0e511daca38c6a
Farhan Asif <f.asif2010@gmail.com>
2020-05-05 02:17:04 +0600

    config

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \Pusher\Pusher;
 
 class HomeController extends Controller
 {
@@ -24,5 +25,20 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function authenticate(Request $request) {
+        $socketId = $request->socket_id;
+        $channelName = $request->channel_name;
+
+        $pusher = new Pusher(env('PUSHER_APP_KEY'), env('PUSHER_APP_SECRET'), env('PUSHER_APP_ID'), [
+            'cluster' => 'ap2',
+            'encrypted' => true
+        ]);
+
+        $presence_data = ['name' => auth()->user()->name];
+        $key = $pusher->presence_auth($channelName, $socketId, auth()->id(), $presence_data);
+
+        return response($key);
     }
 }
--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import MediaHandler from '../MediaHandler';
+import Pusher from 'pusher-js';
+import Peer from 'simple-peer';
+
+const APP_KEY = '52bec963c8c77852129e';
 
 export default class App extends Component {
     constructor() {
@@ -11,13 +15,24 @@ export default class App extends Component {
             otherUserId: false
         }
 
-        this.mediaHandler = new MediaHandler()
+        this.user = window.user;
+        this.user.stream = null;
+        this.peers = {};
+
+        this.mediaHandler = new MediaHandler();
+        this.setupPusher();
+
+        this.callTo = this.callTo.bind(this);
+        this.setupPusher = this.setupPusher.bind(this);
+        this.startPeer = this.startPeer.bind(this);
     }
 
     componentWillMount(){
         this.mediaHandler.getPermissions()
         .then((stream) => {
             this.setState({ hasMedia: true });
+            this.user.stream = stream;
+
             try{
                 this.myVideo.srcObject = stream;
             }
@@ -30,9 +45,82 @@ export default class App extends Component {
         })
     }
 
+    setupPusher() {
+        Pusher.logToConsole = true;
+        this.pusher = new Pusher(APP_KEY, {
+            authEndpoint: '/pusher/auth',
+            cluster: 'ap2',
+            auth: {
+                params: this.user.id,
+                headers: {
+                    'X-CSRF-Token': window.csrfToken
+                }
+            }
+        });
+
+        this.channel = this.pusher.subscribe('webrtc-user');
+
+        this.channel.bind(`client-signal-${this.user.id}`, (signal) => {
+            let peer = this.peers[signal.userId];
+
+            // if peer is not already exists, we got an incoming call
+            if(peer === undefined) {
+                this.setState({otherUserId: signal.userId});
+                peer = this.startPeer(signal.userId, false);
+            }
+
+            peer.signal(signal.data);
+        });
+    }
+
+    startPeer(userId, initiator = true) {
+        const peer = new Peer({
+            initiator,
+            stream: this.user.stream,
+            trickle: false
+        });
+
+        peer.on('signal', (data) => {
+            this.channel.trigger(`client-signal-${userId}`, {
+                type: 'signal',
+                userId: this.user.id,
+                data: data
+            });
+        });
+
+        peer.on('stream', (stream) => {
+            try {
+                this.userVideo.srcObject = stream;
+            } catch (e) {
+                this.userVideo.src = URL.createObjectURL(stream);
+            }
+
+            this.userVideo.play();
+        });
+
+        peer.on('close', () => {
+            let peer = this.peers[userId];
+            if(peer !== undefined) {
+                peer.destroy();
+            }
+
+            this.peers[userId] = undefined;
+        });
+
+        return peer;
+    }
+
+    callTo(userId) {
+        this.peers[userId] = this.startPeer(userId);
+    }
+
+
     render() {
         return (
             <div className="app">
+                {[1,2,3,4].map((userId) => {
+                    return this.user.id !== userId ? <button key={userId} onClick={() => this.callTo(userId)}>Call {userId}</button> : null;
+                })}
                 <div className="video-container">
                     <video className="my-video" ref={(ref) => {this.myVideo = ref}}></video>
                     <video className="user-video" ref={(ref) => {this.userVideo = ref}}></video>
